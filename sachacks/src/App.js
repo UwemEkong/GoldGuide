5f2696cae3d6ce22587ee30060e7f555810e3712
Noelle Szombathy <business.noellered@gmail.com>
2021-02-20 14:25:31 -0800

    add react router

--- sachacks/src/App.js
+++ sachacks/src/App.js
@@ -2,8 +2,10 @@ import './App.css';
 import Resources from './pages/Resources';
 import { AppProvider } from '@shopify/polaris';
 import Stepper from './layouts/Stepper';
+import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Results from './layouts/Results';
+import Landing from './pages/Landing';
 
 function App() {
   const resources = <Resources/>
@@ -11,10 +13,22 @@ function App() {
   return (
     <div className="App">
       <AppProvider>
+        <Router>
         <div className="step-container">
-          {/* <Stepper step1={resources}/> */}
-          <Results/>
+          <Switch>
+
+          <Route exact path="/">
+            <Landing/>
+          </Route>
+          <Route exact path="/find-resources">
+            <Stepper step1={resources}/>
+          </Route>
+
+          </Switch>
+
+
         </div>
+        </Router>
       </AppProvider>
     </div>
 
