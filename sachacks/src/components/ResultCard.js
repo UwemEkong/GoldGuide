d281396941ddc59a907b64559868fb93f8ee5fbe
Noelle Szombathy <business.noellered@gmail.com>
2021-02-20 15:50:40 -0800

    add results cards

--- sachacks/src/components/ResultCard.js
+++ sachacks/src/components/ResultCard.js
@@ -1,7 +1,19 @@
 import React from 'react';
 
-const ResultCard = () => {
+const ResultCard = ({ name, services }) => {
+    
+    // const handleClick = () => {
+    //     console.log(isSelected)
+    //     isSelected ? setIsSelected(false) : setIsSelected(true);
+    //     onSelect(value)
+    // }
 
+    return(
+        <div className="card card-wide col-lg-12">
+            <span className="card-title">{name}</span>
+            {services.map((service) => <span className="badge">{service}</span>)}
+        </div>
+    )
 }
 
 export default ResultCard;
--- sachacks/src/layouts/Results.js
+++ sachacks/src/layouts/Results.js
@@ -1,8 +1,14 @@
 import React, {useState} from 'react';
-import LoadingBars from '../assets/loading-bars.svg'
+import LoadingBars from '../assets/loading-bars.svg';
+import resourceList from '../resources.json';
+import ResultCard from '../components/ResultCard';
 
 const Results = () => {
+
     const [isLoading, setIsLoading] = useState(false)
+
+    const resourcesJSON = resourceList.resourceList
+
     return(
         <>
         {isLoading ? <div className="loading">
@@ -10,8 +16,16 @@ const Results = () => {
             <img className="mx-auto" src={LoadingBars}/> 
             </div>
             <h3>Finding Results for you</h3>
-            </div> : <h1>Results</h1>}
+            </div> :         
+            <div className="mt-5">
+                <h3>Here's what we found:</h3>
+                <div className="result-card-container col-lg-4">
+                    {resourcesJSON.map((result) => <ResultCard name={result.name} services={result.services}></ResultCard>)}
+                </div>
+            </div>
+        }
         </>
+
     )
 }
 
